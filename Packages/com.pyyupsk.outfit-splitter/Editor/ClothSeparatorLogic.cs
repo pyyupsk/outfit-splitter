c3a01d9bc7444e90c66feac47aae19a992b59afb
Pongsakorn Thipayanate <contact@fasu.dev>
2026-08-28 11:05:52 +0700

    feat: keep original, create _Original backup, disable original renderers

--- Packages/com.pyyupsk.outfit-splitter/Editor/ClothSeparatorLogic.cs
+++ Packages/com.pyyupsk.outfit-splitter/Editor/ClothSeparatorLogic.cs
@@ -66,10 +66,27 @@ namespace Pyyupsk.OutfitSplitter.Editor
 
                 if (!partialMode)
                 {
+                    // Create a disabled backup of the original avatar
                     foreach (var smr in renderers)
                     {
                         if (smr != null)
-                            Undo.DestroyObjectImmediate(smr.gameObject);
+                        {
+                            var avatarRoot = GetAvatarRoot(smr.transform);
+                            var originalBackup = UnityEngine.Object.Instantiate(avatarRoot.gameObject, avatarRoot.parent);
+                            originalBackup.name = avatarRoot.name + "_Original";
+                            originalBackup.SetActive(false);
+                            Undo.RegisterCreatedObjectUndo(originalBackup, "Create Original Backup");
+                            break; // Only need one backup per avatar
+                        }
+                    }
+                    // Disable original renderers
+                    foreach (var smr in renderers)
+                    {
+                        if (smr != null)
+                        {
+                            Undo.RecordObject(smr, "Disable Original Renderer");
+                            smr.enabled = false;
+                        }
                     }
                 }
                 else
